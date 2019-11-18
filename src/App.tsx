4ccb2f398851d734593ef4b37e5377cbf90ca22d
Jonghoon <huni452@naver.com>
2019-11-18 19:24:51 +0900

    Create Expense.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { data } from "./lib/data.json";
 import Household from "./components/Household";
 import Daily from "./components/Daily";
+import Expense from "./components/Expense";
 
 function App() {
   return (
@@ -16,12 +17,13 @@ function App() {
             total={daily.expenses.reduce((acc, cur) => acc + cur.price, 0)}
           >
             {daily.expenses.map((expense, idx) => (
-              <tr key={idx}>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-                <td>4</td>
-              </tr>
+              <Expense
+                key={idx}
+                index={idx + 1}
+                name={expense.name}
+                price={expense.price}
+                place={expense.place}
+              ></Expense>
             ))}
           </Daily>
         ))}
